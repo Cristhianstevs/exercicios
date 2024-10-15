cdcaabbf2d2ce51ee647ee70b9db1a1d7907fac1
Cristhian Esteves <c.gasparesteves@gmail.com>
2024-10-14 23:06:28 -0300

    Atualizações exercicios

---
+++ js/exercicios_gpt/01 - Iniciante/01 - Sintaxe Básica/03 - Operadores Aritméticos/operadoresAritimeticosLogicos.js
@@ -0,0 +1,30 @@
+/*
+
+Operadores Aritméticos e Lógicos
+
+Operadores Aritimétocos:
+
++ (Adição)
+- (Subtração)
+* (Multiplicação)
+/ (Divisão)
+% (Módulo(Resto da divisão))
+** (Exponenciação)
+
+Operadores Lógicos:
+
+*/
+
+let idade = 17
+
+/*
+if(idade >= 18) {
+    mensagem = "maior de idade"
+}
+else {
+    mensagem = "menor de idade"
+}
+*/
+let mensagem = (idade >= 18) ? "maior de idade" : "menor de idade";
+
+console.log(`Você tem ${idade} anos e é ${mensagem}`)
